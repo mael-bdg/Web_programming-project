e278462699fe051827e65c4e1f2996fee2f1a651
Jade <jade.lavigne205@gmail.com>
2026-05-20 16:10:23 +0900

    modification from (name) to (first name) and (last name) for reset

--- script.js
+++ script.js
@@ -17,21 +17,28 @@ const formMessage = document.getElementById("formMessage");
 contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const name = document.getElementById("name").value.trim();
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
 
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (name === "" || email === "" || message === "") {
+    if (firstName === "" || lastName === "" || email === "" || message === "") {
+
         formMessage.textContent = "Please fill in all fields.";
         formMessage.style.color = "red";
+
     } else if (!email.match(emailPattern)) {
+
         formMessage.textContent = "Please enter a valid email address.";
         formMessage.style.color = "red";
+
     } else {
+
         formMessage.textContent = "Message sent successfully!";
         formMessage.style.color = "green";
+
         contactForm.reset();
     }
 });
